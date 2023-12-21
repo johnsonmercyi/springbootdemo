3c9fe572bad2a3b3e9ca7b87e6dc950df16427cc
iykeori <oricletus@gmail.com>
2023-12-21 00:42:16 -0400

    workinf on return sales controller

--- src/main/java/com/soft/springbootdemo/controller/ReturnSaleController.java
+++ src/main/java/com/soft/springbootdemo/controller/ReturnSaleController.java
@@ -3,15 +3,21 @@ package com.soft.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soft.springbootdemo.dto.ReturnSaleDTO;
 import com.soft.springbootdemo.model.ReturnSale;
 import com.soft.springbootdemo.service.returnsale.ReturnSaleService;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -25,5 +31,14 @@ public class ReturnSaleController {
     public ResponseEntity<Collection<ReturnSale>> findAllReturnSales() {
         return ResponseEntity.ok(returnSaleService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ReturnSale>> findSaleById(@PathVariable UUID id){
+        return ResponseEntity.ok(returnSaleService.findById(id));
+    }
     
+    @PostMapping
+    public ResponseEntity<ReturnSale> saveReturnSale(@RequestBody ReturnSaleDTO returnSaleDTO){
+        return ResponseEntity.ok(returnSaleService.save(returnSaleDTO));
+    }
 }
